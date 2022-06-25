b8efedc8ce08df96e4435c7394bd2f295ffbd01e
Pires <60065025+RPires27@users.noreply.github.com>
2022-06-25 20:50:35 +0100

    update

--- src/main/kotlin/com/istec/projetoistecwebservice/cliente/Cliente.kt
+++ src/main/kotlin/com/istec/projetoistecwebservice/cliente/Cliente.kt
@@ -1,7 +1,6 @@
 package com.istec.projetoistecwebservice.cliente
 
 import com.binance.connector.client.impl.SpotClientImpl
-import com.binance.connector.client.impl.spot.Market
 import com.istec.projetoistecwebservice.keys.Keys
 
 
@@ -9,7 +8,6 @@ import com.istec.projetoistecwebservice.keys.Keys
 class Cliente {
     val keys = Keys()
     val cliente = SpotClientImpl(keys.api_key,keys.secret_api_key,keys.binannce_api)
-    val cliente_testnet = SpotClientImpl(keys.testnet_api_key,keys.testnet_secret_api_key,keys.testnet_binance_api)
     val arroz = LinkedHashMap<String,Any>()
 
 
